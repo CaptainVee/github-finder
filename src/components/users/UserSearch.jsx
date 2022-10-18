44495b6fc591c68cbc9c9730d0671df8dd330b95
CaptainVee <captainvee3@gmail.com>
2022-10-18 22:51:29 +0100

    search functionality

---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,50 @@
+
+import {useState, useContext} from 'react'
+import GithubContext from '../../context/github/GithubContext'
+
+function UserSearch() {
+    const [text, setText] = useState('')
+
+    const {users, searchUsers, clearUsers} = useContext(GithubContext)
+
+    const handleChange = (e) => setText(e.target.value)
+    const handleClear = (e) => clearUsers()
+    const handleSubmit = (e) => {
+        if (text === ''){
+            console.log(e.target.value)
+            alert(e)
+        }else{
+            console.log(e.target.value)
+            searchUsers(text)
+        }
+
+        e.preventDefault()
+    }
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+        
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <div className="relative">
+                        <input value={text} onChange={handleChange} type="text" placeholder="Search" className="w-full pr-40 bg-gray-200 input input-lg text-black" />
+                        <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">Go</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        {/* Only show clear button if there are users */}
+        {users.length > 0 && (
+            <div>
+                <button onClick={handleClear} className="btn btn-ghost btn-lg">Clear</button>
+            </div>
+        
+        )}
+
+
+    </div>
+  )
+
+}
+export default UserSearch
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,30 +16,42 @@ export const GithubProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    const fetchUsers = async () => {
+    // Get search results
+    const searchUsers = async (user) => {
         setLoading()
-        const response = await fetch(`${GITHUB_URL}/users`
+
+        const params = new URLSearchParams({
+            q: user
+        })
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`
         // {headers: {
         //     Authorization: `token ${GITHUB_TOKEN}`
         // }}
         )
-        const data = await response.json()
+        const {items} = await response.json()
 
         dispatch({
             type: 'GET_USERS',
-            payload: data
+            payload: items
         })
     }
+
 
     // Set loading
     const setLoading = () => dispatch({
         type: 'SET_LOADING'
     })
 
+    // Set loading
+    const clearUsers = () => dispatch({
+        type: 'CLEAR_USERS',
+    })
+
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers
+        searchUsers,
+        clearUsers
     }}>
         { children }
     </GithubContext.Provider>
